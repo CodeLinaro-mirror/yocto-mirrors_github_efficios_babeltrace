87d68f2aafa6ef1119c5f88602e8ff4af7632ac9
Philippe Proulx <eeppeliteloop@gmail.com>
2026-05-15 05:41:52 -0400

    tests: bt_tests_utils.py: remove unused cli_params_from_obj()
    Probably forgot to remove it from this module when I moved CLI stuff
    to `bt_tests_cli_utils.py`.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I0ed2557918871671307d9206b7f31b80707ab1e8
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/17803
    Reviewed-by: Simon Marchi <simon.marchi@efficios.com>

--- tests/utils/python/bt_tests_utils.py
+++ tests/utils/python/bt_tests_utils.py
@@ -195,36 +195,6 @@ def run(
     return res
 
 
-# Formats `obj` as a `babeltrace2` CLI `--params` value string.
-#
-# `obj` may be `None`, a boolean, a number, a string, a list, or
-# a dictionary.
-#
-# This function doesn't escape special characters in strings.
-def cli_params_from_obj(obj: Any, is_root: bool = True) -> str:
-    if obj is None:
-        return "null"
-    elif isinstance(obj, bool):
-        return "yes" if obj else "no"
-    elif isinstance(obj, int):
-        return str(obj)
-    elif isinstance(obj, float):
-        return str(obj)
-    elif isinstance(obj, str):
-        return f'"{obj}"'
-    elif isinstance(obj, list):
-        return f"[{', '.join((cli_params_from_obj(item, False) for item in typing.cast(List[Any], obj)))}]"
-    elif isinstance(obj, dict):
-        items = ", ".join(
-            f"{k}={cli_params_from_obj(v, False)}"
-            for k, v in typing.cast(Dict[str, Any], obj).items()
-        )
-
-        return items if is_root else f"{{{items}}}"
-    else:
-        assert False
-
-
 # Any component type
 _AnyCompT = Union[
     bt2._GenericSourceComponentConst,
